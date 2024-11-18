311ede2a02925f526c1208a54c0d4e7ade15b6b6
JuaniGit <jlategana@itba.edu.ar>
2024-11-18 16:21:08 +0000

    feat: Add TopBar

--- app/src/main/java/com/example/blueberry/registerCard.kt
+++ app/src/main/java/com/example/blueberry/registerCard.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun RegisterCard() {
